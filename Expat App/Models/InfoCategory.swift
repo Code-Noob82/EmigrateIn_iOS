c83a612e237184f388f7577a010f4f0ebb59f96d
Code-Noob82 <baki.dominik@googlemail.com>
2025-04-24 14:09:17 +0200

    Model struct according to Firestore Database

---
+++ Expat App/Models/InfoCategory.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCategory.swift
+//  Expat App
+//
+//  Created by Dominik Baki on 24.04.25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct InfoCategory: Codable, Identifiable {
+    @DocumentID var id: String? // Mappt die Firestore Document-ID (z.B. prep_de) automatisch
+    let title: String
+    let subtitle: String? // Optional, falls bei weiterer Entwicklung in Firestore kein subtitle existiert.
+    let iconName: String? // Swift-Code Image(systemName: category.iconName ?? "placeholder.icon")
+    let order: Int
+}
