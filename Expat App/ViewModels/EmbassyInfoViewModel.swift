ad91beaa7b8208c8f650bdf5d8bc9757f1553a33
Code-Noob82 <baki.dominik@googlemail.com>
2025-05-12 18:50:50 +0200

    Issue #15

---
+++ Expat App/ViewModels/EmbassyInfoViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  EmbassyInfoViewModel.swift
+//  Expat App
+//
+//  Created by Dominik Baki on 12.05.25.
+//
+
+import Foundation
+
+class EmbassyInfoViewModel: ObservableObject {
+    
+}
---
+++ Expat App/Views/EmbassyInfo/EmbassyInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  EmbassyInfoView.swift
+//  Expat App
+//
+//  Created by Dominik Baki on 12.05.25.
+//
+
+import SwiftUI
+
+struct EmbassyInfoView: View {
+    var body: some View {
+        Text("Hallo")
+    }
+}
+
+#Preview {
+    EmbassyInfoView()
+}
